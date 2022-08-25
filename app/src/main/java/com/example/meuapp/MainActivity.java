593801fee7a2b3e367fa5771320cbc05f0b5b86c
Gilson Augusto Helfer <ghelfer@UNISC.BR>
2022-08-24 21:58:44 -0300

    Initial commit

---
+++ app/src/main/java/com/example/meuapp/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.meuapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Spinner spinner;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        spinner = findViewById(R.id.spinner1);
+
+        String [] array = {"a","b","c"};
+
+        ArrayAdapter adapter = new ArrayAdapter<>(this,
+                R.layout.meu_spinner_item1, array);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(),array[i],
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.meu_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        switch (item.getItemId()){
+
+            case R.id.item1:
+                Intent intent = new Intent(MainActivity.this,
+                        MainActivity2.class);
+                startActivity(intent);
+                return true;
+            case R.id.item2:
+                Toast.makeText(getApplicationContext(),"menu item2",
+                        Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item3:
+                Toast.makeText(getApplicationContext(),"menu item3",
+                        Toast.LENGTH_SHORT).show();
+                return true;
+        }
+
+        return false;
+    }
+}
---
+++ app/src/main/java/com/example/meuapp/MainActivity2.java
@@ -0,0 +1,44 @@
+package com.example.meuapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity2 extends ListActivity
+        implements AdapterView.OnItemClickListener {
+
+    List<String> listagem;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        ListView listView = getListView();
+        listView.setOnItemClickListener(this);
+
+        listagem = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            listagem.add("valor" + i);
+        }
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, listagem);
+        setListAdapter(adapter);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        String item = adapterView.getItemAtPosition(i).toString();
+        String item2 = listagem.get(i);
+        Toast.makeText(this,item,Toast.LENGTH_LONG).show();
+    }
+}
